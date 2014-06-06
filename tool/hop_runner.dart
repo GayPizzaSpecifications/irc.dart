48de30afa339d3a3e924088cb6d15b47ed9e5cce
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-06 09:36:53 -0400

    Update Hop Runner

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,10 +1,12 @@
 library hop_runner;
 
+import "dart:io";
+
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
 import 'package:hop_docgen/hop_docgen.dart';
 
 void main(List<String> args) {
-    addTask("docs", createDocGenTask("."));
+    addTask("docs", createDocGenTask("../dartdoc-viewer"));
     runHop(args);
 }
