7943b852245dae104064fd457e55b0972d398bf9
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-06 11:56:50 -0400

    Add an actually useful hop build by generating docs.

---
+++ tool/docgen.dart
@@ -0,0 +1,45 @@
+part of hop_runner;
+
+Task createDocGenTask(String path, {compile: true, Iterable<String> excludes: null, include_sdk: false, include_deps: false}) {
+    return new Task((TaskContext context) {
+        var args = [];
+
+        if (excludes != null) {
+            for (String exclude in excludes) {
+                context.fine("Excluding Library: ${exclude}");
+                args.add("--exclude-lib=${exclude}");
+            }
+        }
+
+        if (compile) {
+            args.add("--compile");
+        }
+
+        if (include_sdk) {
+            args.add("--include-sdk");
+        } else {
+            args.add("--no-include-sdk");
+        }
+
+        if (include_deps) {
+            args.add("--include-dependent-packages");
+        } else {
+            args.add("--no-include-dependent-packages");
+        }
+
+        args.addAll(context.arguments.rest);
+
+        args.add(path);
+
+        context.fine("using argments: ${args}");
+
+        return Process.start("docgen", args).then((process) {
+            return inheritIO(process);
+        }).then((code) {
+            print("Exit Code");
+            if (code != 0) {
+                context.fail("docgen exited with the status code ${code}");
+            }
+        });
+    });
+}
--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,10 +1,18 @@
 library hop_runner;
 
+import 'dart:async';
 import "dart:io";
 
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
+import 'package:quiver/async.dart';
+import 'package:quiver/io.dart';
+import "package:yaml/yaml.dart" hide loadYamlStream, loadYamlNode;
+
+part 'docgen.dart';
+part 'utils.dart';
 
 void main(List<String> args) {
+    addTask("docs", createDocGenTask("."));
     runHop(args);
 }
