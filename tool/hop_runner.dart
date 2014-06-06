3f1dd7e634f4a9f0ba0ebbccbf7415769ce54695
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-06 09:48:43 -0400

    Dump Useless Stuff

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -4,9 +4,7 @@ import "dart:io";
 
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
-import 'package:hop_docgen/hop_docgen.dart';
 
 void main(List<String> args) {
-    addTask("docs", createDocGenTask("../dartdoc-viewer"));
     runHop(args);
 }
