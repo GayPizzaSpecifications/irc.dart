24445ca8dfb7b530634c8b71d80992d502d19da4
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-09 16:14:40 -0400

    Remove a Commented out line of code

--- lib/irc.dart
+++ lib/irc.dart
@@ -10,7 +10,6 @@ import 'package:event_bus/event_bus.dart' show EventType, EventBus;
 export "bot/bot.dart";
 
 part 'client.dart';
-//part 'bot.dart';
 part 'events.dart';
 part 'types.dart';
 part 'colors.dart';
