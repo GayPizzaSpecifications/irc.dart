c632ff03406199c07ada70954dfcd043a3830cbb
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-13 20:48:04 -0400

    add automatic version incrementing

--- tool/version.dart
+++ tool/version.dart
@@ -3,16 +3,47 @@ part of hop_runner;
 Task createVersionTask() {
     return new Task((TaskContext ctx) {
         var file = new File("pubspec.yaml");
-        if (ctx.arguments.rest.length != 1) {
-            ctx.severe("usage: ./build version <version>");
-            return;
-        }
         return new Future(() {
             var content = file.readAsStringSync();
             var pubspec = loadYaml(content);
             var old = pubspec["version"];
-            content = content.replaceAll(old, ctx.arguments.rest[0]);
+
+            var next = null;
+            
+            if (ctx.arguments.rest.length != 1) {
+                next = guessNextVersion(old);
+            } else {
+                next = ctx.arguments.rest[0];
+            }
+
+            content = content.replaceAll(old, next);
             file.writeAsStringSync(content);
+            ctx.info("Updated Version: v${old} => v${next}");
         });
     });
+}
+
+String guessNextVersion(String old) {
+    if (old.contains("dev")) {
+        String dev = old.substring(0, old.lastIndexOf("."));
+        int devNumber = int.parse(old.substring(old.lastIndexOf(".") + 1));
+        devNumber++;
+        return dev + "." + devNumber.toString();
+    } else {
+        return incrementVersion(old);
+    }
+}
+
+String incrementVersion(String old) {
+    List<String> split = old.split(".");
+    int major = int.parse(split[0]);
+    int minor = int.parse(split[1]);
+    int bugfix = int.parse(split[2]);
+    if (bugfix == 9) {
+        bugfix = 0;
+        minor++;
+    } else {
+        bugfix++;
+    }
+    return "${major}.${minor}.${bugfix}";
 }
