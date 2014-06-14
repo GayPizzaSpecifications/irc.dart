50c18a0649dc7743e12653b3806bb243c18411e3
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-14 13:10:26 -0400

    Build Script: analyze examples

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -15,7 +15,7 @@ part 'version.dart';
 
 void main(List<String> args) {
     addTask("docs", createDocGenTask("."));
-    addTask("analyze", createAnalyzerTask(["lib/irc.dart"]));
+    addTask("analyze", createAnalyzerTask(["lib/irc.dart", "example/log.dart", "example/debug.dart", "example/example.dart"]));
     addTask("version", createVersionTask());
     addTask("publish", createProcessTask("pub", args: ["publish", "-f"], description: "Publishes a New Version"));
     addChainedTask("check", ["analyze"]);
