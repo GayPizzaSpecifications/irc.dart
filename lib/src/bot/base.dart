12cfaa055f0087614dadb7994a51aa57b0ca931e
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-24 11:46:45 -0400

    Added Documentation for 'Bot' class

--- lib/src/bot/base.dart
+++ lib/src/bot/base.dart
@@ -4,19 +4,43 @@ part of irc.bot;
  * Base Class for Bots
  */
 abstract class Bot {
+  /**
+   * Gets the Client
+   */
   Client client();
 
+  /**
+   * Connects the Bot
+   */
   void connect() => client().connect();
 
+  /**
+   * Disconnects the Bot
+   */
   void disconnect() => client().disconnect();
 
+  /**
+   * Gets a Channel
+   */
   Channel channel(String name) => client().channel(name);
 
+  /**
+   * Registers a Handler
+   */
   bool register(handler) => client().register(handler);
 
+  /**
+   * Joins a Channel
+   */
   void join(String channel) => client().join(channel);
 
+  /**
+   * Parts a Channel
+   */
   void part(String channel) => client().part(channel);
 
+  /**
+   * Sends a Message
+   */
   void message(String target, String message) => client().message(target, message);
 }
