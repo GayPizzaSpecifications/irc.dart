a242393026acae29428f5c32f5d8c3f0569a20af
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-27 17:09:46 -0400

    Remove Old Sink Implementation in irc.parser

--- lib/src/parser/base.dart
+++ lib/src/parser/base.dart
@@ -15,39 +15,4 @@ abstract class IrcParser extends Converter<String, Message> {
    */
   @override
   Message convert(String line);
-
-  /**
-   * Provides support for chunked parsing
-   */
-  @override
-  IrcParserSink startChunkedConversion(Sink<String> sink) {
-    if (sink is! StringConversionSink) {
-      sink = new StringConversionSink.from(sink);
-    }
-    return new IrcParserSink(this, sink);
-  }
-}
-
-/**
- * Sink Implementation for IRC Parsers
- */
-class IrcParserSink extends StringConversionSinkBase {
-  final IrcParser _converter;
-  final StringConversionSink _sink;
-
-  IrcParserSink(this._converter, this._sink);
-
-  @override
-  void addSlice(String chunk, int start, int end, bool isLast) {
-    var result = this._converter.convert(chunk);
-    _sink.add(result);
-    if (isLast)
-      _sink.close();
-  }
-
-  /**
-   * Closes the Sink
-   */
-  @override
-  void close() => super.close();
 }
