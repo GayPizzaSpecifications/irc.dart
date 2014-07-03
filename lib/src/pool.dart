5fc8e89485a52ff4f0ea01d29aa922fb19ad1931
Kenneth Endfinger <kaendfinger@gmail.com>
2014-07-03 16:42:08 -0400

    Formatting and Fixes in ClientPool

--- lib/src/pool.dart
+++ lib/src/pool.dart
@@ -2,26 +2,16 @@ part of irc;
 
 class ClientPool {
   List<Client> clients;
-  
+
   int addClient(BotConfig config) {
     var client = new Client(config);
     clients.add(client);
     return clients.indexOf(client);
   }
-  
-  void connectAll() => clients.forEach((client) => client.connect());
-  
-  void disconnectAll([String reason = null, bool force = false]) => clients.forEach((client) => client.disconnect(reason: reason, force: force));
 
-  void message(String target, String message) {
-    clients.forEach((client) {
-      client.message(target, message);
-    });
-  }
-  
-  void register(handler) {
-    clients.forEach((client) {
-      client.register(handler);
-    });
-  }
+  void connectAll() => forEach((client) => client.connect());
+  void disconnectAll([String reason = null, bool force = false]) => forEach((client) => client.disconnect(reason: reason, force: force));
+  void message(String target, String message) => forEach((client) => client.message(target, message));
+  void register(handler) => forEach((client) => client.register(handler));
+  void forEach(void action(Client client)) => clients.forEach(action);
 }
