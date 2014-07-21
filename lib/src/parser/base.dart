88ed56307634cf9e056f39d835d04757fce574d0
Kenneth Endfinger <kaendfinger@gmail.com>
2014-07-21 13:46:58 -0400

    Tweak Documentation to use spaces for the code highlighting

--- lib/src/parser/base.dart
+++ lib/src/parser/base.dart
@@ -3,11 +3,9 @@ part of irc.parser;
 /**
  * Base class for IRC Parsers
  *
- * ```
- * var parser = new SomeIrcParser();
- * var message = parser.convert(":some.server PRIVMSG #SomeChannel :Some Message");
- * // Use Message Instance
- * ```
+ *      var parser = new SomeIrcParser();
+ *      var message = parser.convert(":some.server PRIVMSG #SomeChannel :Some Message");
+ *      // Use Message Instance
  */
 abstract class IrcParser extends Converter<String, Message> {
 
